cc78aa4fcf104f3b81f6dfb28cfaf21b7ae4aaab
Daniel_Dmitrachkov <d.dmitrachkov@g.nsu.ru>
2023-05-04 12:42:23 +0700

    add scene.json generator in main and sim_objects parser in model

--- solar_system/main.cpp
+++ solar_system/main.cpp
@@ -1,6 +1,59 @@
 #include "controller/solar_controller.h"
+#include <fstream>
+
+void CreateScene() {
+  json all = {
+      {"SCENE", {
+        {"CAMERA",
+          {
+              {"position", {0.0, 0.0, 0.0}},
+              {"rotation", {0.0, 0.0, 0.0}},
+              {"fov", 60.0},
+              {"near_plane", 0.1},
+              {"far_plane", 100.0},
+          }},
+         {"FPS", 90}
+        }
+      },
+      {"OBJECTS", {
+        {
+          {"ID", "Planet"}, 
+          {"ATTRIBUTES", {
+            {"type", "planet"},
+            {"mass", 1.0},
+            {"radius", 1.0},
+            {"coordinates", {5.0, 5.0, 0.0}},
+            {"GRAPHICS", {
+              {"model", "sphere"},
+              {"texture", "earth.png"},
+              {"material", "default"}
+              }}
+            }
+          }
+        },
+        {
+          {"ID", "Sun"},
+          {"ATTRIBUTES", {
+            {"type", "star"},
+            {"mass", 10.0},
+            {"radius", 2.0},
+            {"coordinates", {0.0, 0.0, 0.0}},
+            {"GRAPHICS", {
+              {"model", "sphere"},
+              {"texture", "sun.png"},
+              {"material", "light_source"}
+              }}
+            }
+          }
+        }}
+      }
+  };
+  std::ofstream file("scene.json");
+    file << all.dump(4);
+}
 
 int main(int argc, char** argv) {
+  CreateScene();
   SolarController controller(argv[0]);
   controller.Start();
   return 0;
--- solar_system/model/solar_model.cpp
+++ solar_system/model/solar_model.cpp
@@ -1,6 +1,25 @@
 #include "solar_model.h"
+  
 
 void SolarSystem::Initialize() {
+  std::ifstream f("scene.json");
+  json all_data = json::parse(f);
+  for (const auto& obj : all_data["OBJECTS"].get<json>()) {
+    sim_objects.emplace(obj["ID"].get<std::string>(),
+      obj["ATTRIBUTES"].get<json>());
+  }
+  // json gr_info = {
+  //   {"Planet", {
+  //     //primitive, material,...
+  //   }}
+  // };
+  // json planet1 = {
+  //   {"type", "planet"},
+  //   {"mass", 4.0},
+  //   {"radius", 3.0},
+  //   {"coordinates", {0.0, 0.0, 0.0}}
+  // };
+  // sim_objects.emplace("Planet", planet1);
   std::cout << "Solar System initialized" << std::endl;
 }
 
